704616ff4ab9d48941b051101f703fa78847afd7
nadja <nadja.kalenda31@gmail.com>
2024-12-26 10:52:05 +0100

    add Homework01

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@
 
 
 import './App.css'
-import Lesson01 from './lessons/lesson_01/lesson01'
-import Lesson02 from './Lesson02/Lesson02';
+import Homework01 from './homeworks/homework01/Homework01';
+// import Lesson01 from './lessons/lesson_01/lesson01'
+// import Lesson02 from './Lesson02/Lesson02';
 
 function App() {
   
 
   return (
-    <>
-      
-      <Lesson01 />
-      <Lesson02 />
-    </>
-  )
-}
+    <div>
+        <Homework01 />
+    </div>
+);
+};
 
-export default App
+export default App;
+
---
+++ src/homeworks/homework01/Homework01.jsx
@@ -0,0 +1,27 @@
+
+
+import './homework01.css';
+
+
+function Homework01()
+    {
+        return(
+         <>
+               <div className="profile-card">
+            <img 
+                 src="/image/foto1.png" alt="My Profile" className="profile-image" 
+            
+            />
+            <h2 className="profile-name">Nadja</h2>
+            <p className="profile-hobby">
+                Я увлекаюсь программированием, чтением книг и плаванием. Люблю путешествовать и открывать новые места.
+            </p>
+        </div>
+                
+                </>
+               
+    
+            );
+        };
+        
+        export default Homework01;
